7e57e209765d8d034b8eba2775bd92074cec5add
CsikosMate <mate.csikos@student.uni-miskolc.hu>
2026-03-23 13:39:43 +0100

    feat: Video in window, dont save

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ import cv2
 import random
 import os
 
-def create_random_clip_opencv(input_path, output_path, clip_length_sec):
+def create_random_clip_opencv(input_path, clip_length_sec):
     try:
         # Videó megnyitása
         cap = cv2.VideoCapture(input_path)
@@ -31,34 +31,29 @@ def create_random_clip_opencv(input_path, output_path, clip_length_sec):
         print(f"Videó adatai: {fps} FPS, Felbontás: {width}x{height}")
         print(f"Kivágás kezdete: {start_frame}. képkocka (kb. {start_frame/fps:.2f}. másodperc)")
 
-        # Videóíró inicializálása (MP4 formátumhoz)
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
-        out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
-
         # Ugrás a kisorsolt kezdő képkockára
         cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+        delay = int(1000/fps)
 
         # Képkockák beolvasása és kiírása
-        print("Kivágás és mentés folyamatban (ez OpenCV-vel eltarthat egy ideig)...")
         for i in range(frames_to_extract):
             ret, frame = cap.read()
             if not ret:
                 print("Figyelmeztetés: A vártnál hamarabb véget ért a videó.")
                 break
-            out.write(frame)
-
-        print(f"Sikeresen elmentve (hang nélkül): {output_path}")
+            cv2.imshow("Clip",frame)
+            if cv2.waitKey(delay) & 0xFF == 27:
+                break;
+        cv2.destroyAllWindows()
 
     except Exception as e:
         print(f"Hiba történt: {e}")
     finally:
         # Erőforrások felszabadítása
         if 'cap' in locals(): cap.release()
-        if 'out' in locals(): out.release()
 
 # Paraméterek
-BEMENETI_VIDEO = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "videos", "test1_5min.mp4")
-KIMENETI_VIDEO = "veletlen_reszlet_opencv.mp4"
+BEMENETI_VIDEO = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "videos", "test4_11min.mp4")
 KIVAGAS_HOSSZA = 20 # másodperc
 
-create_random_clip_opencv(BEMENETI_VIDEO, KIMENETI_VIDEO, KIVAGAS_HOSSZA)
+create_random_clip_opencv(BEMENETI_VIDEO, KIVAGAS_HOSSZA)
